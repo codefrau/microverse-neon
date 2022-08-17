1fb5400e6faf5ef32040e30af25b9434650a3918
Vanessa Freudenberg <vanessa@codefrau.net>
2022-08-17 12:33:30 -0700

    Initial commit

---
+++ apiKey.js
@@ -0,0 +1,4 @@
+/* Copy this into a file named apiKey.js */
+const apiKey = '1_yo5yrgo0tboo77rkcpoizjl1xn7dqdte9rlv40pv';
+const appId = 'io.croquet.vanessa.microverse';
+export default {apiKey, appId};
---
+++ behaviors/croquet/avatarEvents.js
@@ -0,0 +1,101 @@
+class AvatarPawn {
+    setup() {
+        if (!this.isMyPlayerPawn) {return;}
+
+        this.addFirstResponder("pointerTap", {ctrlKey: true, altKey: true}, this);
+        this.addEventListener("pointerTap", this.pointerTap);
+
+        this.addFirstResponder("pointerDown", {ctrlKey: true, altKey: true}, this);
+        this.addLastResponder("pointerDown", {}, this);
+        this.addEventListener("pointerDown", this.pointerDown);
+
+        this.addFirstResponder("pointerMove", {ctrlKey: true, altKey: true}, this);
+        this.addLastResponder("pointerMove", {}, this);
+        this.addEventListener("pointerMove", this.pointerMove);
+
+        this.addLastResponder("pointerUp", {ctrlKey: true, altKey: true}, this);
+        this.addEventListener("pointerUp", this.pointerUp);
+
+        this.addLastResponder("pointerWheel", {ctrlKey: true, altKey: true}, this);
+        this.addEventListener("pointerWheel", this.pointerWheel);
+
+        this.removeEventListener("pointerDoubleDown", "onPointerDoubleDown");
+        this.addFirstResponder("pointerDoubleDown", {shiftKey: true}, this);
+        this.addEventListener("pointerDoubleDown", this.addSticky);
+
+        this.addLastResponder("keyDown", {ctrlKey: true}, this);
+        this.addEventListener("keyDown", this.keyDown);
+
+        this.addLastResponder("keyUp", {ctrlKey: true}, this);
+        this.addEventListener("keyUp", this.keyUp);
+    }
+
+    startMotion(dx, dy) {
+        this.spin = Microverse.q_identity();
+        this.velocity = Microverse.v3_zero();
+        this.say("startFalling");
+        if (dx || dy) this.updateMotion(dx, dy);
+    }
+
+    endMotion(_dx, _dy) {
+        this.activeMMotion = false;
+        this.spin = Microverse.q_identity();
+        this.velocity = Microverse.v3_zero();
+    }
+
+    updateMotion(dx, dy) {
+        const JOYSTICK_V = 0.000030;
+        const MAX_V = 0.015;
+        const MAX_SPIN = 0.0004;
+
+        let v = dy * JOYSTICK_V;
+        v = Math.min(Math.max(v, -MAX_V), MAX_V);
+
+        const yaw = dx * (this.isMobile ? -2.5 * MAX_SPIN : -MAX_SPIN);
+        this.spin = Microverse.q_euler(0, yaw ,0);
+        this.velocity = [0, 0, v];
+        this.maybeLeavePresentation();
+    }
+
+    teardown() {
+        if (!this.isMyPlayerPawn) {return;}
+        console.log("avatar event handler detached");
+        this.removeFirstResponder("pointerTap", {ctrlKey: true, altKey: true}, this);
+        this.removeEventListener("pointerTap", this.pointerTap);
+
+        this.removeFirstResponder("pointerDown", {ctrlKey: true, altKey: true}, this);
+        this.removeLastResponder("pointerDown", {}, this);
+        this.removeEventListener("pointerDown", this.pointerDown);
+
+        this.removeFirstResponder("pointerMove", {ctrlKey: true, altKey: true}, this);
+        this.removeLastResponder("pointerMove", {}, this);
+        this.removeEventListener("pointerMove", this.pointerMove);
+
+        this.removeLastResponder("pointerUp", {ctrlKey: true, altKey: true}, this);
+        this.removeEventListener("pointerUp", this.pointerUp);
+
+        this.removeLastResponder("pointerWheel", {ctrlKey: true, altKey: true}, this);
+        this.removeEventListener("pointerWheel", this.pointerWheel);
+
+        this.removeEventListener("pointerDoubleDown", "onPointerDoubleDown");
+        this.removeFirstResponder("pointerDoubleDown", {shiftKey: true}, this);
+        this.removeEventListener("pointerDoubleDown", this.addSticky);
+
+        this.removeLastResponder("keyDown", {ctrlKey: true}, this);
+        this.removeEventListener("keyDown", this.keyDown);
+
+        this.removeLastResponder("keyUp", {ctrlKey: true}, this);
+        this.removeEventListener("keyUp", this.keyUp);
+    }
+}
+
+export default {
+    modules: [
+        {
+            name: "AvatarEventHandler",
+            pawnBehaviors: [AvatarPawn],
+        }
+    ]
+}
+
+/* globals Microverse */
